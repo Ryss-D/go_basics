fabe6fe5e8251efe7af163009c2248eda6ce2e5b
Ryss-D <ryss-d@protonmail.com>
2021-07-11 13:39:15 -0500

    stated whit go channel

---
+++ channels/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    links := []string{
+        "http://google.com",
+        "http://facebook.com",
+        "http://golang.org",
+        "http//amazon.com",
+}
+
+    for _, link := range(links) {
+        
+
+    }
+
+}
