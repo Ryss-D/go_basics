9888de3f02c41807f658bee5c74a7f334a1152f0
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-06-21 16:58:08 -0500

    fix: wronk fuction names

--- interfaces/main.go
+++ interfaces/main.go
@@ -3,40 +3,38 @@ package main
 import "fmt"
 
 type bot interface {
-    getGreeting() string
-    //any type that succes the condition of have a function getGreeting() and return a string now can access to fuctions 
-    //of type bot something like a abstract class
+	getGreeting() string
+	//any type that succes the condition of have a function getGreeting() and return a string now can access to fuctions
+	//of type bot something like a abstract class
 }
-// the default interface sintax is 
+
+// the default interface sintax is
 // type bot interface {
 //  function (type of  arguments) (type of retuns)
 // on go we can talk about concrete types and interfaces types(we cant create interface types from scratch or directrly)
 type englishBot struct{}
 type spanishBot struct{}
 
-
 func main() {
-    eb := englishBot{}
-    sb := spanishBot{}
+	eb := englishBot{}
+	sb := spanishBot{}
 
-    printGreeting(eb)
-    printGreeting(sb)
+	printGreeting(eb)
+	printGreeting(sb)
 }
 
 func printGreeting(b bot) {
-    fmt.Println(b.getGreeding())
+	fmt.Println(b.getGreeting())
 }
 
-
-func (eb englishBot) getGreeding() string {
-    //func (englishBot) will by totatlly equivalent
-    //TO DO VERY CUSTOM ENGLISH LOGIC 
-    return "Hello there" }
-
-func (spanishBot) getGreeding() string {
-    //func (sb spanishBot) will by totatlly equivalent
-    //TO DO VERY CUSTOM SPANISH LOGIC 
-    return "Hola"
+func (eb englishBot) getGreeting() string {
+	//func (englishBot) will by totatlly equivalent
+	//TO DO VERY CUSTOM ENGLISH LOGIC
+	return "Hello there"
 }
 
-
+func (sb spanishBot) getGreeting() string {
+	//func (sb spanishBot) will by totatlly equivalent
+	//TO DO VERY CUSTOM SPANISH LOGIC
+	return "Hola"
+}
